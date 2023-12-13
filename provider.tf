866aa6652ec32dee564352cbf09af48d7a03be10
Abhishek Sharma <abhishek.sharma59244@gmail.com>
2023-12-13 15:55:02 +0530

    backend

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+   aws = {
+     source = "hashicorp/aws"
+     version = "~> 4.16"
+   }
+ }
+
+  required_version = ">= 1.2.0"
+  backend "s3" {
+    bucket = "jenkns-bckt-2"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+ region  = "us-east-1"
+ profile = "default"
+}
+
