03760f538835197e8ec1b89a16e982717a6a6f9d
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2023-12-14 01:21:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "My_bucket" {
-  bucket = "jenkins-bckt-3"  # Set a globally unique bucket name
-  acl    = "private"         # Access control for the bucket, e.g., private, public-read
+  bucket = "jenkns-bckt-2"  # Set a globally unique bucket name
+  acl    = "private"  # Access control for the bucket, e.g., private, public-read
 
   tags = {
     Name        = "MyBucket"
